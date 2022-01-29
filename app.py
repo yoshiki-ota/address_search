64993141e178f27e1e0c078134509d23d86ad7a2
yoshiki-ota <football_18.y_o@icloud.com>
2022-01-29 15:00:02 +0900

    入力部分作成

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import requests
 
-url = "https://zipcloud.ibsnet.co.jp/api/search?zipcode=0250067"
+zipcode = "0250067"
+# zipcode = input("郵便番号<ハイフンなし7桁>は？:")
+url = f"https://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}"
 
 response = requests.get(url)
 print(response.text)
