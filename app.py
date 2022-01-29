d8e00120ad299ba96a0fd736529fb4068daab5fc
yoshiki-ota <football_18.y_o@icloud.com>
2022-01-29 15:58:37 +0900

    さらに処理をひとまとまりに

--- app.py
+++ app.py
@@ -6,17 +6,14 @@ def main():
 
 
 zipcode = "0250067"
-# zipcode = input("郵便番号<ハイフンなし7桁>は？:")
+# zipcode = input("郵便番号<ハイフン無し7桁>は?")   # 郵便番号<ハイフン無し7桁>は?
+
 url = f"https://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}"
 
 response = requests.get(url)
-
-# jsonでdicに変換した↓
-results = response.json()['results']
-
-pref_name = (results[0]['address1'])
-city_name = (results[0]['address2'])
-town_name = (results[0]['address3'])
+pref_name = response.json()['results'][0]['address1']
+city_name = response.json()['results'][0]['address2']
+town_name = response.json()['results'][0]['address3']
 
 address = f"{pref_name}{city_name}{town_name}"
 print(address)
